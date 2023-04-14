af24cd9188af49626ab23670487a4cbc9ae3d358
Teryn Morgan <tpmorgan@iu.edu>
2023-04-13 23:15:26 -0400

    deploy

--- setup.sh
+++ setup.sh
@@ -1,8 +1,6 @@
-mkdir -p ~/.streamlit/
-
-echo "\
-[server]\n\
-headless = true\n\
-enableCORS=false\n\
-port = $PORT\n\
+mkdir -p ~/.streamlit
+echo "[server]
+headless = true
+port = $PORT
+enableCORS = false
 " > ~/.streamlit/config.toml
